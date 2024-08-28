10f8b5c7ca7b7dbe284edba95efa515054feb985
Dean <imadeureadis@gmail.com>
2024-08-28 20:21:35 +0200

    implemented view

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
-var express = require('express');
-var path = require('path');
-var logger = require('morgan');
-var bodyParser = require('body-parser');
-var redis = require('redis');
+const redis = require('redis');
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
 
-var app = express();
+// Create a new Redis client
+const client = redis.createClient();
 
-// view engine setup
+client.on('connect', () => {
+  console.log('Redis Server Connected...');
+});
+
+// Connect to the Redis server
+client.connect().catch(console.error);
+
+const app = express();
+
+// View engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -15,13 +25,26 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Route to get tasks
+app.get('/', async function (req, res) {
+  const title = 'Task List';
 
-app.get('/', function(req, res){
-    res.send('Welcome');
+  try {
+    // Use the new 'lRange' method with 'await'
+    const tasks = await client.lRange('tasks', 0, -1);
+    res.render('index', {
+      title: title,
+      tasks: tasks,
+    });
+  } catch (err) {
+    console.error('Error fetching tasks:', err);
+    res.status(500).send('Error fetching tasks');
+  }
 });
 
-
-app.listen(3000);
-console.log('Server Started On Port 3000...');
+// Start the server
+app.listen(3000, () => {
+  console.log('Server Started On Port 3000');
+});
 
 module.exports = app;
